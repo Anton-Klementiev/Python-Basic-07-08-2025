3f7da7c028ff971f9b65c9a534201386b13d13df
Anton <klementiev.anton93@gmail.com>
2025-08-17 12:12:38 +0300

    Modified the output to be the whole number

--- DZ_2.2.py
+++ DZ_2.2.py
@@ -6,4 +6,4 @@ fourth_number = user_input // 10 % 10
 fifth_number = user_input %10
 
 output = (fifth_number*10000 + fourth_number*1000 + third_number*100 + second_number*10 + first_number*1)
-print("Inserted number in reverse order: ",output)
+print("Inserted number in reverse order:",output)
