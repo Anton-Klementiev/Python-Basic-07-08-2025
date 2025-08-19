52db6488f56035c84bae1c069853baa308c7582b
Anton <klementiev.anton93@gmail.com>
2025-08-19 23:36:38 +0300

    seems to be done

--- DZ_3.1.py
+++ DZ_3.1.py
@@ -1,21 +1,20 @@
-#to check if it's only supposed to be an integer - change to float if it isn't
-first_number = int(input("Insert First Number : "))
-second_number = int(input("Insert Second Number : "))
+first_number = input("Insert First Number : ")
+second_number = input("Insert Second Number : ")
 arithmetic_operator = input("Insert Arithmetic Operator (can be +, -, * or / ): ")
 
 if arithmetic_operator == "/" and second_number == 0:
     print('Error - Cannot divide by zero')
 elif arithmetic_operator == "+":
-    answer = first_number + second_number
+    answer = float(first_number) + float(second_number)
     print(first_number, arithmetic_operator, second_number, "=", answer)
 elif arithmetic_operator == "-":
-    answer = first_number - second_number
+    answer = float(first_number) - float(second_number)
     print(first_number, arithmetic_operator, second_number, "=", answer)
 elif arithmetic_operator == "*":
-    answer = first_number * second_number
+    answer = float(first_number) * float(second_number)
     print(first_number, arithmetic_operator, second_number, "=", answer)
 elif arithmetic_operator == "/":
-    answer = first_number / second_number
+    answer = float(first_number) / float(second_number)
     print(first_number, arithmetic_operator, second_number, "=", answer)
 else: print('Input Error')
 
