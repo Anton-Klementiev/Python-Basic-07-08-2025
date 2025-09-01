62e10186c3ddba372e9c658eb5d3b4774906dc6d
Anton <klementiev.anton93@gmail.com>
2025-09-01 11:30:36 +0300

    fixed an issue with using ranges

--- DZ6.2.py
+++ DZ6.2.py
@@ -9,14 +9,16 @@ else:
     total_hours = total_mins // 60
     hours = str(total_hours % 24).zfill(2)
     days = total_hours // 24
-    if days in range(5,20) or days % 10 == 0:
+    if days in (5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20) or days % 10 == 0:
         word_for_day = 'днів'
-    elif days % 10 in range(2, 4):
+    elif days % 10 in (2,3,4):
         word_for_day = 'дні'
-    elif days % 10 in range(5, 10):
+    elif days % 10 in (5,6,7,8,9):
         word_for_day = 'днів'
     elif days % 10 == 1 or days == 1:
         word_for_day = 'день'
+    else:
+        word_for_day = 'днів'
 
     print(f"{user_input} seconds is: {days} {word_for_day}, {hours}:{mins}:{seconds}")
 
