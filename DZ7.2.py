7d9907d3436a7745871707af3fb37682e22805cb
Anton <klementiev.anton93@gmail.com>
2025-09-02 14:28:00 +0300

    seems to be working

---
+++ DZ7.2.py
@@ -0,0 +1,16 @@
+text = input("Please enter a text: ")
+def correct_sentence(text):
+    a = text[0]
+    b = text[-1]
+    if a.isupper() == False:
+        text = a.upper() + text[1:]
+    if b != ".":
+        text += "."
+    return text
+
+#assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+#assert correct_sentence("hello") == "Hello.", 'Test2'
+#assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+#assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+#assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+print('ОК')
